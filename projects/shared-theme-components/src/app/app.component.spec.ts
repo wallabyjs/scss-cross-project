fb246d5a8da4d7c921ad698c25680a238d2ab057
Nikita Govorov <nikita.govorov@gmail.com>
2022-08-11 13:07:28 +1000

    components samples

--- projects/shared-theme-components/src/app/app.component.spec.ts
+++ projects/shared-theme-components/src/app/app.component.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+function getBackgroundColor(element: Element) {
+  function convert(rgb: string) {
+    const result = rgb.match(
+      /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/
+    );
+    function hex(x: string) {
+      return ('0' + parseInt(x).toString(16)).slice(-2);
+    }
+
+    if (!result) {
+      return '';
+    }
+
+    return '#' + hex(result[1]) + hex(result[2]) + hex(result[3]);
+  }
+  return convert(
+    window.getComputedStyle(element, null).getPropertyValue('background-color')
+  );
+}
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,6 +44,13 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shared-theme-components app is running!');
+    const element = compiled.querySelector('span');
+    expect(element).toBeDefined();
+    expect(element?.textContent).toContain(
+      'shared-theme-components app is running!'
+    );
+    if (element) {
+      expect(getBackgroundColor(element)).toBe('#c6538c');
+    }
   });
 });
